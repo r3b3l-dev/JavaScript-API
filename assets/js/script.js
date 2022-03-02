613f38a78ab62a31d76433f0d35addc3d71c2aa6
r3b3l-dev <wayne.manning@tuta.io>
2022-03-02 15:09:31 +0000

    Add core core file structure already prefilled for project

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "Q-9oqOjdYCEaABnz2M90PUSR6BA";
+const API_URL = "https://ci-jshint.herokuapp.com/api/";
+const resultsModal = new bootstrap.Modal(document.getElementsById("resultsModal"))
